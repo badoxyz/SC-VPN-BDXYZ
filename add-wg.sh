3332323999bf0fd63aaaa0546f0163b48bb3e902
badoxyz <87416136+badoxyz@users.noreply.github.com>
2021-07-15 14:45:11 +0700

    Add files via upload

--- add-wg.sh
+++ add-wg.sh
@@ -84,4 +84,6 @@ AllowedIPs = $CLIENT_ADDRESS/32" >>"/etc/wireguard/$SERVER_WG_NIC.conf"
 	echo -e "Wireguard	: http://$MYIP:81/$CLIENT_NAME.conf"
 	echo -e "==============================="
 	echo -e "Expired On      : $exp"
+	echo -e "==============================="
+	echo -e "Script By Badoxyz"
 	rm -f /root/wg0-client-$CLIENT_NAME.conf
