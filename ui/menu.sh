64f40874fbcf2691272e04f3b12d056c6625cc97
badoxyz <87416136+badoxyz@users.noreply.github.com>
2021-08-19 04:49:06 +0700

    Update menu.sh

--- ui/menu.sh
+++ ui/menu.sh
@@ -39,7 +39,7 @@ echo -e   "   9\e[1;33m)\e[m Panel VLess"
 echo -e   "   10\e[1;33m)\e[m Panel Trojan"
 #echo -e   "   9\e[1;33m)\e[m Panel TrojanGO"
 echo -e   "   \e[1;32m============================================================\e" | lolcat
-echo -e   "                             SYSTEM MENU\e[m" | lolcat 
+echo -e   "                             SYSTEM MENU\e" | lolcat 
 echo -e   "   \e[1;32m============================================================\e" | lolcat
 echo -e   "   11\e[1;33m)\e[m  Add Subdomain Host For VPS"
 echo -e   "   12\e[1;33m)\e[m  Renew Certificate V2RAY"
